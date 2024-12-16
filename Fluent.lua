5794b4e3cbba7ffbf54e3579c4bdd4bbf6bf75f4
Dummyrme <163039266+Dummyrme@users.noreply.github.com>
2024-12-16 22:53:35 +0700

    Update Fluent.lua

--- Fluent.lua
+++ Fluent.lua
@@ -3389,7 +3389,8 @@ local aa = {
 						Size = UDim2.new(1, 0,1 ,10),
 						BackgroundTransparency = 1,
 						AnchorPoint = Vector2.new(0, 0.5),
-						Position = UDim2.new(0, 0,0.5, 0)
+						Position = UDim2.new(0, 0,0.5, 0),
+						Parent = k.Parent.Parent
 					}
 				)
 			local l, m, n =
